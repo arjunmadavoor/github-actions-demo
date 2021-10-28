157955fe6d5defc7eb68ab08d713a1bd71e69e65
ARJUN M <61111547+arjunmdr@users.noreply.github.com>
2021-10-28 18:07:53 +0530

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -11,4 +11,4 @@ class ModelTesting(TestCase):
   def test_post_model(self):
     d = self.api
     self.assertTrue(isinstance(d, Post))
-    self.assertEqual(str(d), 'djangos')
+    self.assertEqual(str(d), 'django')
