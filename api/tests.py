d282dac88281749e6d0e99270ce652f60b0b0064
ARJUN M <61111547+arjunmdr@users.noreply.github.com>
2021-10-28 16:34:53 +0530

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from . models import Post
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+  
+  def setUp(self):
+      self.api = Post.objects.create(title='django', author='django', slug='django')
+      
+  def test_post_model(self):
+    d = self.api
+    self.assertTrue(isinstance(d, Post))
+    self.assertEqual(str(d), 'django')
