94bc1fbbf6ab7cebf39107549113b8d0be1f71ec
arjunmdr <arjunmdrcalicut.gmail.com>
2021-11-07 16:18:48 +0530

    update settings.py

--- githubactions/settings.py
+++ githubactions/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.contrib.messages.middleware.MessageMiddleware'
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
